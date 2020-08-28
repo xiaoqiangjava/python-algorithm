27e0887e148ac5af20189b7fddb200d2839a71fb
easonlzhang <easonlzhang@tencent.com>
2020-08-28 16:26:40 +0800

    二叉树深度，广度遍历，迭代，最大深度

--- tree/TreeNode.py
+++ tree/TreeNode.py
@@ -60,7 +60,7 @@ class Solution:
                 stack.append(root)
                 root = root.left
             else:
-                root = stack.pop(-1).right
+                root = stack.pop().right
 
         return result
 
@@ -76,7 +76,7 @@ class Solution:
                 stack.append(root)
                 root = root.left
             else:
-                top = stack.pop(-1)
+                top = stack.pop()
                 result.append(top.val)
                 root = top.right
         return result
@@ -95,14 +95,53 @@ class Solution:
                 root = root.left
             else:
                 top = stack[-1]
-                if not top.right or last_visit == top.right:    # 当前节点的右子树为null或者已经被访问过
+                if not top.right or last_visit == top.right:  # 当前节点的右子树为null或者已经被访问过
                     result.append(top.val)
-                    stack.pop(-1)
+                    stack.pop()
                     last_visit = top  # 标记最后一个访问的元素
                 else:
                     root = top.right
 
         return result
+
+    @staticmethod
+    def bfs(root: TreeNode) -> List[List[int]]:
+        """
+        广度优先
+        """
+        if not root:
+            return []
+        queue = [root]
+        result = []
+        while queue:
+            size = len(queue)  # 记录当前level元素的个数
+            current_nodes = []
+            while size > 0:  # 处理当前层所有的元素
+                temp = queue.pop(0)
+                current_nodes.append(temp.val)
+                if temp.left:
+                    queue.append(temp.left)
+                if temp.right:
+                    queue.append(temp.right)
+                size -= 1
+            result.append(current_nodes)
+
+        return result
+
+    def max_depth(self, root: TreeNode) -> int:
+        """
+        求一个树的最大深度
+        """
+        return self.calculate_depth(root, 1)
+
+    def calculate_depth(self, root: TreeNode, depth: int) -> int:
+        if not root:
+            return 0
+        if not root.left and not root.right:
+            return depth
+        left_depth = self.calculate_depth(root.left, depth + 1)
+        right_depth = self.calculate_depth(root.right, depth + 1)
+        return max(left_depth, right_depth)
 
 
 if __name__ == '__main__':
@@ -123,3 +162,5 @@ if __name__ == '__main__':
     print(Solution.inOrderTraversal(node))
     print(solution.postOrder(node, []))
     print(Solution.postOrderTraversal(node))
+    print(Solution.bfs(node))
+    print(solution.max_depth(node))
